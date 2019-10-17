49a470a1a996a2de5e3e1f394687e81125fb1f0a
mikeyokeeffe <mikey.okeeffe@unipart.io>
2019-10-17 10:01:51 +0100

    Functioning requests to demoserver

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
+var send_request = (data) => {
+  console.log('Sending ' + data)
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", 'http://10.100.1.143:5000/hello', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    data: data
+}));
+}
+
 window.addEventListener('load', ()=>{
   const sounds=document.querySelectorAll(".sound");
   const pads=document.querySelectorAll(".pads div");
@@ -11,7 +21,13 @@ window.addEventListener('load', ()=>{
     }
   }
 
-});
+  const send_button = document.getElementById("send");
+  send_button.addEventListener('click', () => {
+    console.log('Button clicked')
+    let text = document.getElementById("scan").value
+    send_request({"scan":text})
+  });
 
+});
 
 
